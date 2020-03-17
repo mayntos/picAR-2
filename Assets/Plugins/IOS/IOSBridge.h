b7f400b04d9143b7795538ffba2d4bf753c3668e
Maynard <maynardcsantos@gmail.com>
2020-03-17 13:15:54 -0400

    Move interface code into IOSBridge.h

--- Assets/Plugins/IOS/IOSBridge.h
+++ Assets/Plugins/IOS/IOSBridge.h
@@ -2,11 +2,25 @@
 //  IOSBridge.h
 //  
 //
-//  Created by Maynard Santos on 12/6/17.
+//  Created by Maynard Santos on 3/17/20.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface IOSBridge : NSObject
+
+@end
+
+@interface NonRotatingUIImagePickerController : UIImagePickerController
+
+@end
+
+@interface APLViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>{			//interface for the main view controller for the application.
+
+    UIImagePickerController *imagePickerController;																			//we call this object to manage photolibrary. Eventually we will need to delegate it
+@public
+    const char *callback_game_object_name;																					//the name of the gameobject we will be passing the NSURL to?
+    const char *callback_function_name;																					//once we have passed the NSURL to a game object in the c# script, we pass the NSURL to a function!
+}
 
 @end
