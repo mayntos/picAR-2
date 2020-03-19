11049c13ee8e262ef637f48862a421905dc8a7b1
Maynard <maynardcsantos@gmail.com>
2020-03-18 21:19:14 -0400

    Add UI icon assets and draft bridge to iOS image picker

---
+++ Assets/#Scripts/PhotoAccessManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PhotoAccessManager : MonoBehaviour
+{
+    [DllImport("_Internal")]
+    private static extern void OpenImagePicker(string gameObjectName, string functionName);
+
+    public void OpenImagePicker_Helper()
+    {
+        OpenImagePicker(gameObject.name, "AssignImage");
+    }
+
+    // TODO: determine how the image should be assigned to AR game object.
+    // TODO: test whether the above extern function executes as expected.
+    //private void AssignImage(string path)
+    //{
+
+    //}
